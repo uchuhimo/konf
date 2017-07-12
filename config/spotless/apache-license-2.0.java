d1bd5217bf5690610edd70783526f6a4fe81932e
uchuhimo <uchuhimo@outlook.com>
2017-07-13 04:28:46 +0800

    refactor: add license header

--- config/spotless/apache-license-2.0.java
+++ config/spotless/apache-license-2.0.java
@@ -13,18 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.uchuhimo.konf.source.json
-
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.uchuhimo.konf.source.Source
-import com.uchuhimo.konf.source.SourceProvider
-import java.io.InputStream
-import java.io.Reader
-
-object JsonProvider : SourceProvider {
-    override fun fromReader(reader: Reader): Source =
-            JsonSource(ObjectMapper().readTree(reader))
-
-    override fun fromInputStream(inputStream: InputStream): Source =
-            JsonSource(ObjectMapper().readTree(inputStream))
-}
