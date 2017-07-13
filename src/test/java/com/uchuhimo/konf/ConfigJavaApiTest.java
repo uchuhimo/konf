5d21cbb4e86c1eeec48ef6984161432329cc7324
uchuhimo <uchuhimo@outlook.com>
2017-07-13 23:10:50 +0800

    gradle: disable licenseHeaderFile and use IDE's license support

--- src/test/java/com/uchuhimo/konf/ConfigJavaApiTest.java
+++ src/test/java/com/uchuhimo/konf/ConfigJavaApiTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.uchuhimo.konf;
 
 import static org.hamcrest.MatcherAssert.assertThat;
--- src/test/kotlin/com/uchuhimo/konf/source/json/JsonProviderSpec.kt
+++ src/test/kotlin/com/uchuhimo/konf/source/json/JsonProviderSpec.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.uchuhimo.konf.source.json
 
 import com.natpryce.hamkrest.assertion.assertThat
